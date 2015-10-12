169856b0524bad197bb8b832281719642abc124b
Joseph McConnell <jjmdevelops@gmail.com>
2015-10-11 20:02:24 -0400

    Created and added an overlay to the main map

--- Assets/Scripts/resourceTracker.cs
+++ Assets/Scripts/resourceTracker.cs
@@ -11,17 +11,22 @@ public class resourceTracker : MonoBehaviour {
 		string s = Data.scienceResCount.ToString();
 		string e = Data.espionageResCount.ToString();
 		string d = Data.diplomacyResCount.ToString();
+        string days = Data.dayCounter.ToString();
+
 		Text guiText1 = GameObject.Find("mtext").GetComponent<Text>();
-		guiText1.text = "Military Resources: " + m;
+		guiText1.text = " " + m;
 
 		guiText1 = GameObject.Find("stext").GetComponent<Text>();
-		guiText1.text = "Science Resources: " + s;
+		guiText1.text = " " + s;
 
 		guiText1 = GameObject.Find("etext").GetComponent<Text>();
-		guiText1.text = "Espionage Resources: " + e;
+		guiText1.text = " " + e;
 
 		guiText1 = GameObject.Find("dtext").GetComponent<Text>();
-		guiText1.text = "Diplomatic Resources: " + d;
+		guiText1.text = " " + d;
+
+        guiText1 = GameObject.Find("DayCounter").GetComponent<Text>();
+        guiText1.text = " " + days;
 	}
 	
 	// Update is called once per frame
