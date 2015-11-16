18197924b0b886089014c84f3358f522d2fc45db
Joseph McConnell <jjmdevelops@gmail.com>
2015-11-16 16:19:28 -0500

    Tutorial portion that tells the player about character cooldown is in place and functioning

---
+++ Assets/Scripts/tutorialCooldown.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tutorialCooldown : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Data.dayCounter == 2)
+        {
+            gameObject.SetActive(true);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+	
+	}
+}
